fba6f4dcf02caf37d703f64347097a8a64e48875
Svenson1 <240273@umons.ac.be>
2025-05-19 12:27:03 +0200

    ca marche

--- src/testBasique.py
+++ src/testBasique.py
@@ -9,9 +9,9 @@ trans_speed = 1e19  # bps
 dist = 1e3  # m
 
 #routeur :
-A = Router("A")
-B = Router("B")
-C = Router("C")
+A = Router(1)
+B = Router(2)
+C = Router(3)
 
 #lien entre les routeurs
 AB = Link(A, B, trans_speed, prop_speed, dist, 7)
@@ -21,9 +21,7 @@ AC = Link(A, C, trans_speed, prop_speed, dist, 4)
 A.add_neighbor(B, AB)
 A.add_neighbor(C, AC)
 B.add_neighbor(A, AB)
-B.add_neighbor(C, AC)
 C.add_neighbor(A, AC)
-C.add_neighbor(B, AC)
 
 # Envoi du vecteur de distance
 A.send_vector_to_neighbors(simulator, 0)
