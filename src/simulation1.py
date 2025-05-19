031fbbf67646fb8d3c90d762cfcc57b2b0a08323
rotix2564 <161299574+rotix2564@users.noreply.github.com>
2025-05-19 14:24:58 +0200

    simu

--- src/simulation1.py
+++ src/simulation1.py
@@ -1,6 +1,7 @@
 from Simulator import Simulator
 from Router import Router
 from Link import Link
+from src.Simulator import SimulatorEvent
 from topology_loader import load_topology
 
 simulator = Simulator()
@@ -17,8 +18,8 @@ for link in links_data:
        if router_id not in routers:
            routers[router_id] = Router(router_id)
 
+
 #création des liens
-# Création des liens
 for link in links_data:
     id1, id2 = link["endpoints"]
     router1 = routers[id1]
@@ -31,3 +32,18 @@ for link in links_data:
 
     router1.add_neighbor(router2, temp_link)
     router2.add_neighbor(router1, temp_link)
+
+def update_cost(_link, new_cost, current_time):
+    _link.update_cost(new_cost)
+    _link.router1.send_packet_to_neighbors(simulator, current_time)
+    _link.router2.send_packet_to_neighbors(simulator, current_time)
+
+
+#création des events
+for event in events_data:
+    id1, id2 = event["link"]
+    link_key = tuple(sorted((id1, id2)))
+    if link_key in links:
+        link1 = links[link_key]
+        simulator_event = SimulatorEvent(1, update_cost(link1, event["new_cost"], event["time"]))
+        simulator.add_event(event["time"], lambda: simulator_event)
